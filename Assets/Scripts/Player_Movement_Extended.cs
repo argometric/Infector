dd16b1c87d9ff1d8502d6ea9f30f46eaddf869c9
christian schweighofer <bluecactusweb@gmail.com>
2020-11-07 12:18:58 +0100

    WIP: new scientist character
    Added new scientist character
    Animation still broken.
    Fixed character box collider.

---
+++ Assets/Scripts/Player_Movement_Extended.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player_Movement_Extended : MonoBehaviour
+{
+    public float movementSpeed = 5f;
+    public Rigidbody2D rigBody;
+    Vector2 _movement;
+
+    void Update()
+    {
+        _movement.x = Input.GetAxis("Horizontal");
+        _movement.y = Input.GetAxis("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        rigBody.MovePosition(rigBody.position + _movement * movementSpeed * Time.deltaTime);
+    }
+}
